b0f071f09aeedd160a8c8715104b6a7265c9b1e8
Daniel Alonge <BrownMarble@outlook.com>
2020-09-08 14:06:23 +0100

    add tests/test-utils

---
+++ src/tests/test-utils.tsx
@@ -0,0 +1,14 @@
+import React, { useRef } from 'react';
+import { useProvider } from '../index';
+import { render } from '@testing-library/react';
+
+function Provider ({ children }:{ children: any }) {
+  const stateRef = useRef({});
+  const { Provider, value } = useProvider(stateRef)
+
+  return (<Provider value={value}>{children}</Provider>)
+}
+
+export const customRender = (ui: React.ReactElement, options?: any) => {
+  render(ui, { wrapper: Provider, ...options })
+}
